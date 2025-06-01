59bba6fac27fcfcaa39517d82924c9ac72a4eb87
Qi Li <liq22@tsinghua.org.cn>
2025-05-31 20:44:10 -0400

    fix multiple bugs - 减少dataset 的metadata存储，从而减少内存占用 - dtype 不统一BUG float，<f8,o - network cuda bug - add test yaml in wuzhui - metric bug
    - Implemented a main function to serve as the entry point for the Vbench application.
    - Added argument parsing for configuration file path, experiment notes, and pipeline module.
    - Integrated dynamic loading of the specified pipeline module using importlib.
    - Included a default configuration path for testing purposes.
    - Ensured the pipeline execution results are printed upon completion.

---
+++ main_test.py
@@ -0,0 +1,42 @@
+import argparse
+import importlib
+# 调用默认 pipeline
+# from src.Pipeline_01_default import pipeline
+
+def main():
+    """
+    Vbench 主入口，配置环境变量并调用实验流水线
+
+    """
+    # 加载环境变量配置
+    parser = argparse.ArgumentParser(description="任务流水线")
+    
+    parser.add_argument('--config_path', 
+                        type=str, 
+                        # default= '/home/user/LQ/B_Signal/Signal_foundation_model/Vbench/configs/demo/Single_DG/CWRU.yaml', # CWRU.yaml
+                        # default='/home/user/LQ/B_Signal/Signal_foundation_model/Vbench/configs/demo/Multiple_DG/CWRU_THU_using_ISFM.yaml',
+                        # default='/home/user/LQ/B_Signal/Signal_foundation_model/Vbench/configs/demo/dummy_test.yaml',
+                        default='/home/lq/LQcode/2_project/PHMBench/PHM-Vibench/configs/demo/Multiple_DG/test_wuzhui.yaml',
+                        help='配置文件路径')
+    parser.add_argument('--notes', 
+                        type=str, 
+                        default='',
+                        help='实验备注')
+
+    parser.add_argument('--pipeline', 
+                        type=str, 
+                        default='Pipeline_01_default',
+                        help='实验流水线模块路径')
+    
+    args = parser.parse_args()
+    pipeline = importlib.import_module(f'src.{args.pipeline}')
+    # 执行DG流水线
+    results = pipeline.pipeline(args)
+    print(f"完成所有实验！")
+    
+    return results
+
+if __name__ == "__main__":
+    main()
+
+
