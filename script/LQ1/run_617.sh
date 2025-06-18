6357ef26090e8c3eed6cd5e8920c58f320ec8e44
liq22 <liq22@tsinghua.org.cn>
2025-06-19 03:40:18 +0800

    update

--- script/LQ1/run_617.sh
+++ script/LQ1/run_617.sh
@@ -1,1 +1,6 @@
-CUDA_VISIBLE_DEVICES=1 python main_LQ.py
+CUDA_VISIBLE_DEVICES=5,6,7,8 python main_LQ.py \
+    --config_path script/LQ1/Pretraining/Pretraining_C+P.yaml \
+    --fs_config_path script/LQ1/GFS/GFS_C+M.yaml \
+    --notes "EXP1" \
+    # --pipeline 
+    
