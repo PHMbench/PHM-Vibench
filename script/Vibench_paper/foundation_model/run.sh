811a21f9ec5b4e90f6ea1b0ea435a8ddb764c1ad
Qi Li <liq22@tsinghua.org.cn>
2025-09-06 22:42:27 -0400

    fix: 更新数据目录路径以确保正确的数据加载，调整调试模式配置

---
+++ script/Vibench_paper/foundation_model/run.sh
@@ -0,0 +1,6 @@
+python main_LQ.py --config_path multitask_B_04_Dlinear.yaml
+
+python main_LQ.py --config_path multitask_B_08_PatchTST.yaml
+
+python main_LQ.py --config_path multitask_B_06_TimesNet.yaml
+
