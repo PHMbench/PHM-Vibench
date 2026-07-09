a6ef96f287dcfbf38934d3da23ae2edbcc2ffca1
liq22 <142728467+liq22@users.noreply.github.com>
2026-07-09 17:43:57 +0800

    feat: export UXFD operator attention modules

--- src/model_factory/X_model/UXFD/operator_attention/__init__.py
+++ src/model_factory/X_model/UXFD/operator_attention/__init__.py
@@ -1,4 +1,8 @@
 """Operator-attention components (UXFD)."""
 
-__all__ = []
+from .operator_attention_1d import OperatorAttention1D, OperatorAttentionConfig
 
+__all__ = [
+    "OperatorAttention1D",
+    "OperatorAttentionConfig",
+]
